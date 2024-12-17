f45451d515e7f70e10e3da6b50797f97311ae83f
SP <spcfxda+cdk@gmail.com>
2024-12-17 18:24:45 +0100

    Fix: logic update for node task

--- scripts/tasks/node.ts
+++ scripts/tasks/node.ts
@@ -1,45 +1,29 @@
 import { HardhatRuntimeEnvironment } from "hardhat/types";
-// import { spawn } from "child_process";
 
 export async function node(
     taskArguments: unknown,
     hre: HardhatRuntimeEnvironment,
     runSuper: unknown,
 ): Promise<void> {
-      // const child = require("child_process").spawn('devkit', ["logs"], {
-      //   cwd: process.cwd(),
-      //   detached: false,
-      //   shell: true,
-      //   stdio: "inherit"
-      // });
-      // child.kill()
-
-
-      // const child_process = require('child_process');
-
-      // process.stdin.setRawMode(true);
-      // // process.stdin.on("data", (data) => {});
-
-      // var child = child_process.spawn('devkit', ["start", "--scan"], {stdio: "inherit"});
-      // child.on("exit", (e, code) => {
-      //   process.exit();
-      // });
-
       const { spawnSync } = require('child_process');
 
-      // Define the command and arguments
-      const command = 'devkit'; // Replace this with the desired command
-      const args = ["start"]; // Example arguments
-      
-      // Spawn the child process synchronously
+      // Define the command and arguments      
+      const command = '/usr/local/bin/deno';
+      const args = [
+          '--allow-env',
+          '--allow-read',
+          '--allow-write',
+          '--allow-ffi',
+          '--allow-net=127.0.0.1',
+          '/opt/conflux/src/cli.ts',
+          'start',
+      ];
       const result = spawnSync(command, args, {
-          stdio: 'inherit', // Inherit stdio to show output directly in the console
+          stdio: 'inherit',
       });
       
       // Handle the result of the child process
       if (result.error) {
           console.error(`Error occurred: ${result.error.message}`);
-      } else {
-          console.log(`Child process exited with code ${result.status}`);
       }
 }
